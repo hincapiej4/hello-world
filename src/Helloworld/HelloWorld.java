306a0c2ec60bafba8d20164a2f296c01408f88b9
Juan Hincapie <hincapiej5@gmail.com>
2017-10-24 21:12:34 -0700

    print date

--- src/Helloworld/HelloWorld.java
+++ src/Helloworld/HelloWorld.java
@@ -1,7 +1,10 @@
 package Helloworld;
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello, World!");
+        System.out.println(new Date());
     }
 }
